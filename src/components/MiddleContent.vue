5df1b107d737341d860e2ebc5e5e7372ba01bb4b
wbr <borenwuapple@163.com>
2017-02-25 10:34:02 +0800

    third change

---
+++ src/components/MiddleContent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main col s12 m8 l9 offset-l3">
+    <div class="middle-content container ">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<style>
+  .middle-content.container{
+    margin-top: 35px;
+  }
+</style>
+
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello middle content'
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
